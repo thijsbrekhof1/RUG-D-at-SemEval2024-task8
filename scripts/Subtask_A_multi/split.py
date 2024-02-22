431873d1c0a6e750e413982f93a0ed4366e5c1e2
YuwenZhou99 <144034975+YuwenZhou99@users.noreply.github.com>
2024-02-22 15:10:45 +0100

    Update split.py

--- scripts/Subtask_A_multi/split.py
+++ scripts/Subtask_A_multi/split.py
@@ -1,10 +1,10 @@
 import json
 
-# 输入和输出文件
+# input and output files
 input_file_path = 'subtaskA_train_multilingual.jsonl'
 output_files = {}
 
-# 映射source到语言缩写
+# the cource map
 source_language_map = {
     'wikipedia': 'en',
     'wikihow': 'en',
@@ -20,27 +20,22 @@ source_language_map = {
     'german': 'de',
 }
 
+# split the file based on 'source'
 with open(input_file_path, 'r', encoding='utf-8') as input_file:
     for line in input_file:
-        # 解析JSON数据
         data = json.loads(line)
 
-        # 获取文本和源信息
         source = data.get('source', 'unknown')
 
-        # 获取语言缩写
         language_abbr = source_language_map.get(source, 'unknown')
 
-        # 构建输出文件路径
         output_file_path = f'subtaskA_train_{language_abbr}.jsonl'
 
-        # 如果输出文件尚未打开，则打开它并将其保存在字典中
         if output_file_path not in output_files:
             output_files[output_file_path] = open(output_file_path, 'a', encoding='utf-8')
 
-        # 将记录写入相应的文件
         output_files[output_file_path].write(json.dumps(data) + '\n')
 
-# 关闭所有输出文件
+# close all outout files
 for file in output_files.values():
     file.close()
